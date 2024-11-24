68787f3f7b88763f7082db48026728dc82d250ea
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2024-11-24 01:27:49 +0100

    new page

---
+++ src/views/dashboardPage.vue
@@ -0,0 +1,127 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+import { getUserData } from '@/auth/user'
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const userData = ref({})
+
+onMounted(async () => {
+  try {
+    const user = await getUserData()
+    userData.value = user
+    console.log(userData.value)
+  } catch (error) {
+    console.log(error)
+    router.push('/login')
+  }
+})
+</script>
+
+<template>
+  <NavBar />
+  <br /><br /><br /><br /><br />
+  <br />
+
+  <div class="form-container">
+    <p>Hello {{ userData.email }}</p>
+  </div>
+</template>
+
+<style scoped>
+.form-container {
+  padding: 10%;
+  display: flex;
+}
+
+.form-container img {
+  width: 80%;
+  min-width: 300px;
+  max-width: 500px;
+  display: none;
+}
+
+.form-container .content {
+  padding: 0 20px;
+}
+
+.form-container .content small.error {
+  font-family: 'Montserrat';
+  padding: 10px;
+  background: #fff;
+  display: inline-block;
+  box-shadow: var(--sdw-1);
+  width: 100%;
+  border-bottom: 1px solid red;
+  transform: translateY(-10px);
+  visibility: hidden;
+  opacity: 0;
+  transition: all 0.2s linear;
+}
+
+.form-container .content .title h1 {
+  font-family: 'Poppins';
+}
+
+.form-container .content form {
+  width: 100%;
+  padding: 10px 0;
+}
+
+.form-container .content form .control {
+  width: inherit;
+  padding: 10px 0;
+}
+
+.form-container .content form .control.password {
+  position: relative;
+}
+
+.form-container .content form .control input {
+  width: inherit;
+  border: 2px solid var(--blue);
+  padding: 10px 5px;
+  font-family: 'Montserrat';
+}
+
+.form-container .content form .control.password i {
+  position: absolute;
+  right: 2%;
+  top: calc(25% + 5px);
+}
+
+.form-container .content form button {
+  width: 100%;
+  font-family: 'Poppins';
+  background: var(--blue);
+  color: #fff;
+  padding: 10px;
+}
+
+.form-container img {
+  width: 80%;
+  min-width: 300px;
+  max-width: 500px;
+  display: none;
+}
+
+small.no-account {
+  color: var(--w-grey);
+  display: block;
+  font-family: 'Montserrat';
+  margin-top: 10px;
+  text-align: right;
+}
+
+small.no-account a {
+  text-decoration: underline;
+  color: var(--blue);
+}
+@media (min-width: 900px) {
+  .form-container img {
+    min-width: 400px;
+    display: block;
+  }
+}
+</style>
