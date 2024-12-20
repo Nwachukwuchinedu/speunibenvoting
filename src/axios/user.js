0e3afe4bd4f54380c34431f2f783b7d6ee1d6daf
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2024-12-20 14:33:16 +0100

    added admin data

--- src/axios/user.js
+++ src/axios/user.js
@@ -32,6 +32,31 @@ export const getUserData = async () => {
   }
 }
 
+export const getAdminData = async () => {
+  const token = localStorage.getItem('admin-token')
+  if (!token) {
+    throw new Error('No token found. Please log in.')
+  }
+
+  try {
+    const response = await axios.get('http://localhost:5000/api/admin/me', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error fetching user data:', error)
+    if (error.response && error.response.status === 401) {
+      // Token is invalid or expired
+      localStorage.removeItem('admin-token') // Remove invalid token
+      throw new Error('Invalid or expired token. Please log in again.')
+    }
+    throw error
+  }
+}
+
+
 export const fetchAllPositions = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/position/all`)
