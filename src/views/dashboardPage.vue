db5e0a0970b18fd6c27180481c2b8f4737ea1991
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2025-01-10 13:58:22 +0100

    updated

--- src/views/dashboardPage.vue
+++ src/views/dashboardPage.vue
@@ -250,7 +250,7 @@ const sendVerificationEmail = async () => {
                 />
 
                 <!-- Vote Button -->
-                <button v-if="isLoading">
+                <!-- <button v-if="isLoading">
                   <p>Loading...</p>
                 </button>
                 <button
@@ -271,7 +271,7 @@ const sendVerificationEmail = async () => {
                         ? 'Voted'
                         : 'Vote'
                   }}
-                </button>
+                </button> -->
               </div>
             </div>
           </div>
